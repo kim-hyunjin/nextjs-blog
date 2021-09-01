f688339841440847510a93e0ada9c6e4e81020d7
kim-hyunjin <hyunjin1612@gmail.com>
2021-09-01 11:36:35 +0900

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilsStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+/**
+ * By returning allPostsData inside the props object in getStaticProps,
+ * the blog posts will be passed to the Home component as a prop.
+ */
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -18,6 +32,20 @@ export default function Home() {
           Here is my github url{" "}
           <a href="https://github.com/kim-hyunjin">link</a>
         </p>
+      </section>
+      <section className={`${utilsStyles.headingMd} ${utilsStyles.padding1px}`}>
+        <h2 className={utilsStyles.headingLg}>Blog</h2>
+        <ul className={utilsStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilsStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
